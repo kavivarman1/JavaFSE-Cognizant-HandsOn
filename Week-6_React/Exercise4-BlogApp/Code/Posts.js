6148874944b4332155325711cf339142c4c43e62
kavivarman1 <kavisaravanakumar2004@gmail.com>
2025-07-29 19:14:50 +0530

    Week 6

---
+++ Week-6_React/Exercise4-BlogApp/Code/Posts.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => {
+        const postList = data.map(p => new Post(p.id, p.title, p.body));
+        this.setState({ posts: postList });
+      })
+      .catch(err => {
+        console.error('Error loading posts:', err);
+        this.setState({ hasError: true });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred in Posts component!");
+    console.error(error, info);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
